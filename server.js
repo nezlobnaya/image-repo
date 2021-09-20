66f6ff87610a8877ec05dc5bb838e90c16fde05f
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2021-09-20 13:45:01 -0400

    connected to MongoDB

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import app from './app.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
 
-const port = process.env.PORT || 5000;
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => 
+        console.log(`Mongo Connection successful. Server running → PORT ${PORT}`)))
+    .catch((err) => console.log(`${err} Mongo Connection unsuccessful`));
 
-const server = app.listen(port, () => {
-    console.log(`Server running → PORT ${port}`);
-});
+
 
