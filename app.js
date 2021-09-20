228f964f903b8837d321751f5e7cc04a35f7417a
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2021-09-20 13:14:20 -0400

    Initial commit. Server is up and running

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json({ exceeded: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message
+            }});
+});
+
+export default app;
+
+
+
+
---
+++ server.js
@@ -0,0 +1,9 @@
+import app from './app.js';
+
+
+const port = process.env.PORT || 5000;
+
+const server = app.listen(port, () => {
+    console.log(`Server running → PORT ${port}`);
+});
+
