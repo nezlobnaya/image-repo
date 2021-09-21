46b5cb0eb93fac7b0f66a23c2432c7295d7d5847
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2021-09-20 23:20:55 -0400

    refactored routes and controller functions

--- controllers/index.js
+++ controllers/index.js
@@ -1,100 +1,77 @@
 import mongoose from 'mongoose';
 import Image from '../models/image.js';
 
-export const getImages = (req, res) => {
+export const getImages = async (req, res) => {
     try {
-        Image.find({}, (err, images) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json(images);
-        });
-    }
-    catch (err) {
-        res.send(err);
+        const images = await Image.find();
+        res.status(200).json(images);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
 };
 
-export const getImage = (req, res) => {
-    try {
-        Image.findById(req.params.id, (err, image) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json(image);
-        });
+export const getImage = async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ message: 'Invalid ID' });
     }
-    catch (err) {
-        res.send(err);
+    try {
+        const image = await Image.findById(req.params.id);
+        if (!image) {
+            return res.status(404).json({ message: 'Image not found' });
+        }
+        res.status(200).json(image);
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
 };
 
-export const postImage = (req, res) => {
+  
+export const postImage = async (req, res) => {
+    const image = req.body;
+    const newImage = new Image(image);
     try {
-        const newImage = new Image(req.body);
-        newImage.save((err, image) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json(image);
-        });
-    }
-    catch (err) {
-        res.send(err);
+        const savedImage = await newImage.save();
+        res.status(201).json(savedImage);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
 };
+   
 
-export const updateImage = (req, res) => {
-    try {
-        Image.findById(req.params.id, (err, image) => {
-            if (err) {
-                res.send(err);
-            }
-            image.title = req.body.title;
-            image.description = req.body.description;
-            image.save((err, image) => {
-                if (err) {
-                    res.send(err);
-                }
-                res.json(image);
-            });
-        });
+export const updateImage = async (req, res) => {
+    const updatedImage = req.body;
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ message: 'Invalid ID' });
     }
-    catch (err) {
-        res.send(err);
+    try {
+        const image = await Image.findByIdAndUpdate(req.params.id, updatedImage, { new: true });
+        if (!image) {
+            return res.status(404).json({ message: 'Image not found' });
+        }
+        res.status(200).json(image);
+
+    } catch (err) {
+        res.status(409).json({ message: err.message });
     }
 };
 
 export const deleteImage = async (req, res) => {
-    const { id } = req.params;
-
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        res.status(400).json({
-            error: 'Invalid ID',
-        });
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ message: 'Invalid ID' });
     }
-
     try {
-        const image = await Image.findById(id);
-
+        const image = await Image.findByIdAndDelete(req.params.id);
         if (!image) {
-            res.status(404).json({
-                error: 'Image not found',
-            });
+            return res.status(404).json({ message: 'Image not found' });
         }
+        res.json({message: 'Image deleted'});
 
-        await image.remove();
-
-        res.json({
-            message: 'Image deleted',
-        });
-    }
-    catch (err) {
-        res.status(500).json({
-            error: err,
-        });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
 };
+
 
 export const deleteAllImages = async (req, res) => {
     try {
@@ -105,186 +82,193 @@ export const deleteAllImages = async (req, res) => {
         });
     }
     catch (err) {
-        res.status(500).json({
-            error: err,
-        });
+        res.status(500).json({ error: err.message });
     }
 };
 
-export const getImagesByUser = (req, res) => {
-    try {
-        Image.find({ user: req.params.id }, (err, images) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json(images);
-        });
+export const getImagesByUser = async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ message: 'Invalid ID' });
     }
-    catch (err) {
-        res.send(err);
+    try {
+        const images = await Image.find({ user: req.params.id });
+        if (!images) {
+            return res.status(404).json({ message: 'Images not found' });
+        }
+        res.status(200).json(images);
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
 };
 
-export const getImagesByTag = (req, res) => {
-    try {
-        Image.find({ tags: req.params.id }, (err, images) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json(images);
-        });
+export const getImagesByTag = async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ message: 'Invalid ID' });
     }
-    catch (err) {
-        res.send(err);
+    try {
+        const images = await Image.find({ tags: req.params.id });
+        if (!images) {
+            return res.status(404).json({ message: 'Images not found' });
+        }
+        res.status(200).json(images);
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
 };
 
-export const getImagesByTitle = (req, res) => {
-    try {
-        Image.find({ title: req.params.id }, (err, images) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json(images);
-        });
+  
+export const getImagesByTitle = async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ message: 'Invalid ID' });
     }
-    catch (err) {
-        res.send(err);
+    try {
+        const images = await Image.find({ title: req.params.id });
+        if (!images) {
+            return res.status(404).json({ message: 'Images not found' });
+        }
+        res.status(200).json(images);
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
 };
 
-export const getImagesByDescription = (req, res) => {
-    try {
-        Image.find({ description: req.params.id }, (err, images) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json(images);
-        });
+
+export const getImagesByDescription = async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ message: 'Invalid ID' });
     }
-    catch (err) {
-        res.send(err);
+    try {
+        const images = await Image.find({ description: req.params.id });
+        if (!images) {
+            return res.status(404).json({ message: 'Images not found' });
+        }
+        res.status(200).json(images);
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
 };
 
-export const getImagesByDate = (req, res) => {
-    try {
-        Image.find({ date: req.params.id }, (err, images) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json(images);
-        });
+
+export const getImagesByDate = async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ message: 'Invalid ID' });
     }
-    catch (err) {
-        res.send(err);
+    try {
+        const images = await Image.find({ date: req.params.id });
+        if (!images) {
+            return res.status(404).json({ message: 'Images not found' });
+        }
+        res.status(200).json(images);
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
 };
 
-export const likeImage = (req, res) => {
-    try {
-        Image.findById(req.params.id, (err, image) => {
-            if (err) {
-                res.send(err);
-            }
-            image.likes++;
-            image.save((err, image) => {
-                if (err) {
-                    res.send(err);
-                }
-                res.json(image);
-            });
-        });
+export const likeImage = async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ message: 'Invalid ID' });
     }
-    catch (err) {
-        res.send(err);
+    try {
+        const image = await Image.findById(req.params.id);
+        if (!image) {
+            return res.status(404).json({ message: 'Image not found' });
+        }
+        image.likeCount += 1;
+        const updatedImage = await image.save();
+        res.status(200).json(updatedImage);
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
 };
 
-export const dislikeImage = (req, res) => {
-    try {
-        Image.findById(req.params.id, (err, image) => {
-            if (err) {
-                res.send(err);
-            }
-            image.likes--;
-            image.save((err, image) => {
-                if (err) {
-                    res.send(err);
-                }
-                res.json(image);
-            });
-        });
+export const dislikeImage = async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ message: 'Invalid ID' });
     }
-    catch (err) {
-        res.send(err);
+    try {
+        const image = await Image.findById(req.params.id);
+        if (!image) {
+            return res.status(404).json({ message: 'Image not found' });
+        }
+        image.likeCount -= 1;
+        const updatedImage = await image.save();
+        res.status(200).json(updatedImage);
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
 };
 
-export const getLikes = (req, res) => {
-    try {
-        Image.findById(req.params.id, (err, image) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json(image.likes);
-        });
+export const getLikes = async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ message: 'Invalid ID' });
     }
-    catch (err) {
-        res.send(err);
+    try {
+        const image = await Image.findById(req.params.id);
+        if (!image) {
+            return res.status(404).json({ message: 'Image not found' });
+        }
+        res.status(200).json(image.likeCount);
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
 };
 
-export const getTags = (req, res) => {
-    try {
-        Image.findById(req.params.id, (err, image) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json(image.tags);
-        });
+export const getTags = async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ message: 'Invalid ID' });
     }
-    catch (err) {
-        res.send(err);
+    try {
+        const image = await Image.findById(req.params.id);
+        if (!image) {
+            return res.status(404).json({ message: 'Image not found' });
+        }
+        res.status(200).json(image.tags);
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
 };
 
-export const addTag = (req, res) => {
-    try {
-        Image.findById(req.params.id, (err, image) => {
-            if (err) {
-                res.send(err);
-            }
-            image.tags.push(req.body.tag);
-            image.save((err, image) => {
-                if (err) {
-                    res.send(err);
-                }
-                res.json(image);
-            });
-        });
+export const addTag = async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ message: 'Invalid ID' });
     }
-    catch (err) {
-        res.send(err);
+    try {
+        const image = await Image.findById(req.params.id);
+        if (!image) {
+            return res.status(404).json({ message: 'Image not found' });
+        }
+        image.tags.push(req.body.tag);
+        const updatedImage = await image.save();
+        res.status(200).json(updatedImage);
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
 };
 
-export const removeTag = (req, res) => {
-    try {
-        Image.findById(req.params.id, (err, image) => {
-            if (err) {
-                res.send(err);
-            }
-            image.tags.pull(req.body.tag);
-            image.save((err, image) => {
-                if (err) {
-                    res.send(err);
-                }
-                res.json(image);
-            });
-        });
+export const removeTag = async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ message: 'Invalid ID' });
     }
-    catch (err) {
-        res.send(err);
+    try {
+        const image = await Image.findById(req.params.id);
+        if (!image) {
+            return res.status(404).json({ message: 'Image not found' });
+        }
+        image.tags.splice(image.tags.indexOf(req.body.tag), 1);
+        const updatedImage = await image.save();
+        res.status(200).json(updatedImage);
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
 };
--- routes/imageRoutes/index.js
+++ routes/imageRoutes/index.js
@@ -4,18 +4,19 @@ import { getImages, getImage, postImage, updateImage, deleteImage, deleteAllImag
 
 const router = express.Router();
 
-
 router.get('/', getImages);
 router.get('/:id', getImage);
 router.post('/', postImage);
 router.put('/:id', updateImage);
 router.delete('/:id', deleteImage);
 router.delete('/', deleteAllImages);
+
 router.get('/user/:id', getImagesByUser);
 router.get('/tag/:id', getImagesByTag);
 router.get('/title/:id', getImagesByTitle);
 router.get('/description/:id', getImagesByDescription);
 router.get('/date/:id', getImagesByDate);
+
 router.put('/like/:id', likeImage);
 router.put('/dislike/:id', dislikeImage);
 router.put('/tag/:id', addTag);
